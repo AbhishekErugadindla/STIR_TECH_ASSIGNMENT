cfd1c175343bb322ae005934440a9725873d4141
Abhishek Erugadindla <128590819+AbhishekErugadindla@users.noreply.github.com>
2025-01-08 19:34:57 +0530

    Update webdriver.py

--- utils/webdriver.py
+++ utils/webdriver.py
@@ -38,6 +38,11 @@ def setup_driver():
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--disable-notifications')
     chrome_options.add_argument('--window-size=1920,1080')
+    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+    chrome_options.add_argument('--disable-infobars')
+    chrome_options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    chrome_options.add_experimental_option('useAutomationExtension', False)
+    chrome_options.add_argument(f'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
     chrome_options.binary_location = "/usr/bin/google-chrome"
     
     # Create and return the driver
